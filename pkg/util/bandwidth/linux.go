1725c6759765cfef9df11cc8b72c7851b4081012
chendotjs <chenyaqi01@baidu.com>
2019-10-30 00:09:05 +0800

    bandwidth: unify IPv4 and IPv6 scanner, add some comments.

--- pkg/util/bandwidth/linux.go
+++ pkg/util/bandwidth/linux.go
@@ -35,21 +35,36 @@ import (
 )
 
 const (
-	maxRowOfMatchLinePerIPv6CIDR = 4
-	hexCIDR8ZeroPadding          = "00000000"
-	hexCIDRZeroIPv6              = "00000000000000000000000000000000/00000000000000000000000000000000"
-
+	// tcFilterIPv4Protocol represents tc filter protocol for IPv4
 	tcFilterIPv4Protocol = "ip"
+	// tcFilterIPv6Protocol represents tc filter protocol for IPv6
 	tcFilterIPv6Protocol = "ipv6"
+	// tcFilterIPv4Priority represents tc filter priority for IPv4
 	tcFilterIPv4Priority = "1"
+	// tcFilterIPv6Priority represents tc filter priority for IPv6
 	tcFilterIPv6Priority = "2"
+	// u32MatchIPv4Protocol represents tc u32 filter protocol for IPv4
 	u32MatchIPv4Protocol = "ip"
+	// u32MatchIPv6Protocol represents tc u32 filter protocol for IPv6
 	u32MatchIPv6Protocol = "ip6"
+
+	// maxRowOfMatchLinePerIPv6CIDR represents the max rows of match lines per IPv6 cidr in `tc filter show dev xxx` output
+	maxRowOfMatchLinePerIPv6CIDR = 4
+	// hexCIDR8ZeroPadding is used to append padding when restore IPv6 store from tc filter match lines
+	hexCIDR8ZeroPadding = "00000000"
+	// hexCIDRZeroIPv6 is all zero IPv6 hex CIDR
+	hexCIDRZeroIPv6 = "00000000000000000000000000000000/00000000000000000000000000000000"
 )
 
 var (
-	classShowMatcher          = regexp.MustCompile(`class htb (1:\d+)`)
+	// classShowMatcher is used to extract classID from `tc class show dev xxx` output.
+	// e.g. class htb 1:10 root prio 0 rate 8Gbit ceil 8Gbit burst 0b cburst 0b
+	classShowMatcher = regexp.MustCompile(`class htb (1:\d+)`)
+	// classAndHandleIPv4Matcher is used to extract classID and handle for IPv4 CIDR from `tc filter show dev xxx` output.
+	// e.g. filter parent 1: protocol ip pref 1 u32 fh 801::800 order 2048 key ht 801 bkt 0 flowid 1:1
 	classAndHandleIPv4Matcher = regexp.MustCompile(`filter parent 1: protocol ip .*fh (\d+::\d+).*flowid (\d+:\d+)`)
+	// classAndHandleIPv6Matcher is used to extract classID and handle for IPv6 CIDR from `tc filter show dev xxx` output.
+	// e.g. filter parent 1: protocol ipv6 pref 2 u32 fh 800::800 order 2048 key ht 800 bkt 0 flowid 1:1
 	classAndHandleIPv6Matcher = regexp.MustCompile(`filter parent 1: protocol ipv6 .*fh (\d+::\d+).*flowid (\d+:\d+)`)
 )
 
@@ -149,6 +164,8 @@ func asciiCIDR(cidr string) (string, error) {
 	return fmt.Sprintf("%s/%d", ip.String(), size), nil
 }
 
+// isIPv6CIDRString uses a tricky way to judge whether a CIDR is IPv6 or not.
+// For CIDR like `::ffff:1.2.3.4/128`, golang's stdlib will treat it as an IPv4 address, while tc uses IPv6 filter and matcher.
 func isIPv6CIDRString(cidr string) bool {
 	return strings.Count(cidr, ":") >= 2
 }
@@ -172,25 +189,23 @@ func (t *tcShaper) findCIDRClass(cidr string) (classAndHandleList [][]string, fo
 
 func (t *tcShaper) findIPv4CIDRClass(hexCIDR string, cmdOutput []byte) (classAndHandleList [][]string, found bool, err error) {
 	spec := fmt.Sprintf("match %s", hexCIDR)
+	outputLines := convertByteSliceToStringSlice(cmdOutput)
 
-	scanner := bufio.NewScanner(bytes.NewBuffer(cmdOutput))
-	filter := ""
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if len(line) == 0 {
-			continue
+	scanner := newlimitRuleScanner(outputLines, false)
+	for scanner.scanLimitRule() {
+		matchLines := scanner.matchLines()
+		// IPv4 only have just one match line
+		if len(matchLines) != 1 {
+			return classAndHandleList, false, fmt.Errorf("unexpected output: %v", matchLines)
 		}
-		if strings.HasPrefix(line, "filter") {
-			filter = line
-			continue
-		}
-		if strings.Contains(line, spec) {
+
+		if strings.Contains(matchLines[0], spec) {
 			// expected tc line:
 			// `filter parent 1: protocol ip pref 1 u32 fh 800::800 order 2048 key ht 800 bkt 0 flowid 1:1` (old version) or
 			// `filter parent 1: protocol ip pref 1 u32 chain 0 fh 800::800 order 2048 key ht 800 bkt 0 flowid 1:1 not_in_hw` (new version)
-			matches := classAndHandleIPv4Matcher.FindStringSubmatch(filter)
+			matches := classAndHandleIPv4Matcher.FindStringSubmatch(scanner.flowidLine())
 			if len(matches) != 3 {
-				continue
+				return classAndHandleList, false, fmt.Errorf("unexpected output from tc: %s %d (%v)", scanner.flowidLine(), len(matches), matches)
 			}
 			resultTmp := []string{matches[2], matches[1]}
 			classAndHandleList = append(classAndHandleList, resultTmp)
@@ -205,37 +220,21 @@ func (t *tcShaper) findIPv4CIDRClass(hexCIDR string, cmdOutput []byte) (classAnd
 func (t *tcShaper) findIPv6CIDRClass(hexCIDR string, cmdOutput []byte) (classAndHandleList [][]string, found bool, err error) {
 	outputLines := convertByteSliceToStringSlice(cmdOutput)
 
-	// Since the match line num of `tc filter show dev xxx` output varies from 0 to 4 for IPv6 cidr,
-	// slow and fast are two indexes that indicate the range of match lines in the output.
-	// When slow finds a flowid line, fast continues to search for the succeeding flowid line or end of output from the position of slow.
-	var slow, fast int = 0, 0
-	for slow < len(outputLines) {
-		matches := classAndHandleIPv6Matcher.FindStringSubmatch(outputLines[slow])
-		if len(matches) != 3 {
-			slow++
-			continue
-		}
-		// expected outputLines[slow]:
-		// filter parent 1: protocol ipv6 pref 2 u32 fh 800::800 order 2048 key ht 800 bkt 0 flowid 1:1
-		fast = slow + 1
-		for fast < len(outputLines) {
-			if !isFilterMatchLine(outputLines[fast]) {
-				break
-			}
-			fast++
-		}
-		// e.g. outputMatchLines:
-		// [match 20010db8/ffffffff at 8 match 86a308d3/ffffffff at 12]
-		// [match 20010da8/ffffffff at 24 match 80006023/ffffffff at 28 match 00000000/ffffffff at 32 match 00000230/ffffffff at 36]
-		outputMatchLines := outputLines[slow+1 : fast]
-		slow = fast
+	scanner := newlimitRuleScanner(outputLines, true)
+	for scanner.scanLimitRule() {
+		matchLines := scanner.matchLines()
 
-		restoreCIDR, err := restoreIPv6HexCIDR(outputMatchLines)
+		// restore hex cidr from match lines
+		restoreCIDR, err := restoreIPv6HexCIDR(matchLines)
 		if err != nil {
 			return classAndHandleList, false, err
 		}
 
 		if restoreCIDR == hexCIDR {
+			matches := classAndHandleIPv6Matcher.FindStringSubmatch(scanner.flowidLine())
+			if len(matches) != 3 {
+				return classAndHandleList, false, fmt.Errorf("unexpected output from tc: %s %d (%v)", scanner.flowidLine(), len(matches), matches)
+			}
 			resultTmp := []string{matches[2], matches[1]}
 			classAndHandleList = append(classAndHandleList, resultTmp)
 		}
@@ -246,19 +245,18 @@ func (t *tcShaper) findIPv6CIDRClass(hexCIDR string, cmdOutput []byte) (classAnd
 	return classAndHandleList, false, nil
 }
 
-// isFilterMatchLine checks whether a line is in the format of `match <cidr> at <number>`
-func isFilterMatchLine(line string) bool {
-	return strings.Contains(line, "match")
-}
-
 // restoreIPv6HexCIDR restores IPv6 CIDR from match lines of tc filter output
-func restoreIPv6HexCIDR(outputMatchLines []string) (string, error) {
-	if len(outputMatchLines) == 0 {
+func restoreIPv6HexCIDR(matchLines []string) (string, error) {
+	if len(matchLines) > maxRowOfMatchLinePerIPv6CIDR {
+		return "", fmt.Errorf("unexpected output: %v", matchLines)
+	}
+	// for all zero IPv6 CIDR "::/0", tc does not output any match line.
+	if len(matchLines) == 0 {
 		return hexCIDRZeroIPv6, nil
 	}
 
 	var ipv6Address, ipv6Mask string
-	for _, line := range outputMatchLines {
+	for _, line := range matchLines {
 		parts := strings.Split(line, " ")
 		// expected line:
 		// match <cidr> at <number>
@@ -274,7 +272,8 @@ func restoreIPv6HexCIDR(outputMatchLines []string) (string, error) {
 		ipv6Mask += ipAndMaskParts[1]
 	}
 
-	rowShort := maxRowOfMatchLinePerIPv6CIDR - len(outputMatchLines)
+	// append 8-zero padding for small CIDR mask size condition
+	rowShort := maxRowOfMatchLinePerIPv6CIDR - len(matchLines)
 	for i := 0; i < rowShort; i++ {
 		ipv6Address += hexCIDR8ZeroPadding
 		ipv6Mask += hexCIDR8ZeroPadding
@@ -458,17 +457,14 @@ func (t *tcShaper) GetCIDRs() ([]string, error) {
 func (t *tcShaper) getIPv4CIDRs(outputLines []string) ([]string, error) {
 	result := []string{}
 
-	for i := 0; i < len(outputLines); i++ {
-		matches := classAndHandleIPv4Matcher.FindStringSubmatch(outputLines[i])
-		if len(matches) != 3 {
-			continue
+	scanner := newlimitRuleScanner(outputLines, false)
+	for scanner.scanLimitRule() {
+		matchLines := scanner.matchLines()
+		// IPv4 only have just one match line
+		if len(matchLines) != 1 {
+			return nil, fmt.Errorf("unexpected output: %v", matchLines)
 		}
-
-		if i+1 >= len(outputLines) {
-			return nil, fmt.Errorf("unexpected output after: %v", outputLines[i])
-		}
-
-		parts := strings.Split(outputLines[i+1], " ")
+		parts := strings.Split(matchLines[0], " ")
 		// expected tc line:
 		// match <cidr> at <number>
 		if len(parts) != 4 {
@@ -478,7 +474,6 @@ func (t *tcShaper) getIPv4CIDRs(outputLines []string) ([]string, error) {
 		if err != nil {
 			return nil, err
 		}
-		i++
 		result = append(result, cidr)
 	}
 
@@ -488,32 +483,11 @@ func (t *tcShaper) getIPv4CIDRs(outputLines []string) ([]string, error) {
 func (t *tcShaper) getIPv6CIDRs(outputLines []string) ([]string, error) {
 	result := []string{}
 
-	// Since the match line num of `tc filter show dev xxx` output varies from 0 to 4 for IPv6 cidr,
-	// slow and fast are two indexes that indicate the range of match lines in the output.
-	// When slow finds a flowid line, fast continues to search for the succeeding flowid line or end of output from the position of slow.
-	var slow, fast int = 0, 0
-	for slow < len(outputLines) {
-		matches := classAndHandleIPv6Matcher.FindStringSubmatch(outputLines[slow])
-		if len(matches) != 3 {
-			slow++
-			continue
-		}
-		// expected outputLines[slow]:
-		// filter parent 1: protocol ipv6 pref 2 u32 fh 800::800 order 2048 key ht 800 bkt 0 flowid 1:1
-		fast = slow + 1
-		for fast < len(outputLines) {
-			if !isFilterMatchLine(outputLines[fast]) {
-				break
-			}
-			fast++
-		}
-		// e.g. outputMatchLines:
-		// [match 20010db8/ffffffff at 8 match 86a308d3/ffffffff at 12]
-		// [match 20010da8/ffffffff at 24 match 80006023/ffffffff at 28 match 00000000/ffffffff at 32 match 00000230/ffffffff at 36]
-		outputMatchLines := outputLines[slow+1 : fast]
-		slow = fast
-
-		restoreCIDR, err := restoreIPv6HexCIDR(outputMatchLines)
+	scanner := newlimitRuleScanner(outputLines, true)
+	for scanner.scanLimitRule() {
+		matchLines := scanner.matchLines()
+		// restore hex cidr from match lines
+		restoreCIDR, err := restoreIPv6HexCIDR(matchLines)
 		if err != nil {
 			return nil, err
 		}
@@ -526,6 +500,92 @@ func (t *tcShaper) getIPv6CIDRs(outputLines []string) ([]string, error) {
 
 	return result, nil
 }
+
+// isFilterMatchLine returns whether a line is in the format of `match <cidr> at <number>`
+func isFilterMatchLine(line string) bool {
+	return strings.Contains(line, "match")
+}
+
+// isFilterFlowidLine returns whether a line is in the format of `filter parent 1: protocol <protocol> pref <prio> u32 fh <handle> order <order> key ht <major> bkt <num> flowid <classid>`
+func isFilterFlowidLine(line string, wantIPv6 bool) bool {
+	var matches []string
+	if wantIPv6 {
+		matches = classAndHandleIPv6Matcher.FindStringSubmatch(line)
+	} else {
+		matches = classAndHandleIPv4Matcher.FindStringSubmatch(line)
+	}
+	if len(matches) != 3 {
+		return false
+	}
+	return true
+}
+
+// limitRuleScanner provides a convenient interface to get data from `tc filter show dev xxx` output.
+// Successive calls to the scanLimitRule method will step to the next limit rule. A limit rule consists of a flowid line and match lines(s).
+// limitRuleScanner supports both IPv4 and IPv6 limit rule. An IPv4 limit rule has only one match line, while an IPv6 limit rule can have 0-4 match lines.
+// A typical IPv4 limit rule as below:
+// filter parent 1: protocol ip pref 1 u32 fh 800::800 order 2048 key ht 800 bkt 0 flowid 1:1
+//  match ac110002/ffffffff at 16
+// A typical IPv6 limit rule as below:
+// filter parent 1: protocol ipv6 pref 2 u32 fh 800::800 order 2048 key ht 800 bkt 0 flowid 1:1
+//  match 20010da8/ffffffff at 24
+//  match 80006023/ffffffff at 28
+//  match 00000000/ffffffff at 32
+//  match 00000230/ffffffff at 36
+type limitRuleScanner struct {
+	tcOutput      []string // the output lines of `tc filter show dev xxx`
+	wantIPv6      bool     // scanLimitRule IPv4 or IPv6
+	idx           int      // index of current line being scanned in the tcOutput
+	flowidLineIdx int      // index of current flowid line in the tcOutput
+	matchlines    []string // current match lines
+}
+
+// newlimitRuleScanner returns a new limitRuleScanner
+func newlimitRuleScanner(cmdOutput []string, wantIPv6 bool) *limitRuleScanner {
+	return &limitRuleScanner{
+		tcOutput: cmdOutput,
+		wantIPv6: wantIPv6,
+	}
+}
+
+// scanLimitRule advances the limitRuleScanner to the next limit rule, which will then be able to call matchLines and flowidLine methods.
+// It returns false when limitRuleScanner reaches the end of the command output.
+func (s *limitRuleScanner) scanLimitRule() bool {
+	for s.idx < len(s.tcOutput) {
+		if !isFilterFlowidLine(s.tcOutput[s.idx], s.wantIPv6) {
+			s.idx++
+			continue
+		}
+
+		// find the most recent flowid line
+		s.flowidLineIdx = s.idx
+		nextFlowidIdx := s.idx + 1
+		// skip match lines, continue to find the next most recent flowid line
+		for nextFlowidIdx < len(s.tcOutput) {
+			if !isFilterMatchLine(s.tcOutput[nextFlowidIdx]) {
+				break
+			}
+			nextFlowidIdx++
+		}
+
+		// match lines are between two flowid lines
+		s.matchlines = s.tcOutput[s.idx+1 : nextFlowidIdx]
+		s.idx = nextFlowidIdx
+		return true
+	}
+	return false
+}
+
+// matchLines returns the most recent match lines found by a call to scanLimitRule.
+func (s *limitRuleScanner) matchLines() []string {
+	return s.matchlines
+}
+
+// flowidLine returns the most recent flowid line found by a call to scanLimitRule.
+func (s *limitRuleScanner) flowidLine() string {
+	return s.tcOutput[s.flowidLineIdx]
+}
+
 func convertByteSliceToStringSlice(data []byte) []string {
 	lines := []string{}
 
@@ -542,8 +602,7 @@ func convertByteSliceToStringSlice(data []byte) []string {
 }
 
 func noSuchQdisc(err error) bool {
-	exitErr, ok := err.(*exec.ExitErrorWrapper)
-	if ok {
+	if exitErr, ok := err.(*exec.ExitErrorWrapper); ok {
 		return exitErr.ExitCode() == 0x2
 	}
 	return false
